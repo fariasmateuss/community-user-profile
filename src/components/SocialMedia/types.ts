a17ea56f94b3c5f4dfaa9dad19256e6907ff4273
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-01-12 21:33:03 -0400

    Create social media component

---
+++ src/components/SocialMedia/types.ts
@@ -0,0 +1,6 @@
+export type NavigationItemProps = {
+  icon: JSX.Element;
+  url: string;
+};
+
+export type NavigationItems = NavigationItemProps[];
--- src/pages/Card/index.tsx
+++ src/pages/Card/index.tsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
-import {
-  AiFillGithub,
-  AiFillLinkedin,
-  AiFillYoutube,
-  AiFillMediumSquare,
-  AiFillTwitterSquare,
-} from 'react-icons/ai';
-import { SiDevDotTo } from 'react-icons/si';
 import { TiLocationArrow } from 'react-icons/ti';
 import { HiOutlineMail } from 'react-icons/hi';
 
 import { ProfileProps } from './types';
 
 import api from '../../services/api';
+
+import SocialMedia from '../../components/SocialMedia';
 
 import './styles.css';
 
@@ -32,124 +26,83 @@ function Card() {
 
   return (
     <main>
-      <div className="container">
-        <div className="card">
-          <div className="header">
-            <div className="hamburger-menu">
-              <div className="center" />
-            </div>
-            <a href="mailto:mateus_vinicius15@outlook.com" className="mail">
-              <HiOutlineMail size={30} />
-            </a>
-            <div className="main">
-              <div className="image">
-                <img
-                  className="avatar"
-                  src={user?.avatar_url}
-                  alt={user?.name}
-                />
+      {user && (
+        <div className="container">
+          <div className="card">
+            <div className="header">
+              <div className="hamburger-menu">
+                <div className="center" />
               </div>
-              <h3 className="name">{user?.name}</h3>
-              <h3 className="sub-location">
-                <span className="icon-location">
-                  <TiLocationArrow />
-                </span>
-                {user?.location}
-              </h3>
-            </div>
-          </div>
-
-          <div className="content">
-            <div className="left">
-              <div className="about-container">
-                <h3 className="title">About</h3>
-                <p className="text">{user?.bio}</p>
-              </div>
-
-              <div className="icons-container">
-                <a
-                  href="https://github.com/fariasmateuss/"
-                  target="blank"
-                  className="icon"
-                >
-                  <AiFillGithub />
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/fariasmateuss/"
-                  target="blank"
-                  className="icon"
-                >
-                  <AiFillLinkedin />
-                </a>
-                <a
-                  href="https://medium.com/@fariasmateuss"
-                  target="blank"
-                  className="icon"
-                >
-                  <AiFillMediumSquare />
-                </a>
-                <a
-                  href="https://twitter.com/fariasmateuss"
-                  target="blank"
-                  className="icon"
-                >
-                  <AiFillTwitterSquare />
-                </a>
-                <a
-                  href="https://dev.to/fariasmateuss"
-                  target="blank"
-                  className="icon"
-                >
-                  <SiDevDotTo />
-                </a>
-                <a
-                  href="https://www.youtube.com/channel/UC2dD4ZlE8zuMoVGOP3jdB8g?sub_confirmation=1"
-                  target="blank"
-                  className="icon"
-                >
-                  <AiFillYoutube />
-                </a>
-              </div>
-
-              <div className="buttons-wrap">
-                <div className="first-wrap">
-                  <a
-                    href="mailto:mateus_vinicius15@outlook.com"
-                    target="blank"
-                    className="first"
-                  >
-                    Contact
-                  </a>
+              <a href="mailto:mateus_vinicius15@outlook.com" className="mail">
+                <HiOutlineMail size={30} />
+              </a>
+              <div className="main">
+                <div className="image">
+                  <img
+                    className="avatar"
+                    src={user.avatar_url}
+                    alt={user.name}
+                  />
                 </div>
-                <div className="second-wrap">
-                  <a
-                    href="https://github.com/fariasmateuss?tab=repositories"
-                    target="blank"
-                    className="second"
-                  >
-                    Projects
-                  </a>
+                <h3 className="name">{user.name}</h3>
+                <h3 className="sub-location">
+                  <span className="icon-location">
+                    <TiLocationArrow />
+                  </span>
+                  {user.location}
+                </h3>
+              </div>
+            </div>
+
+            <div className="content">
+              <div className="left">
+                <div className="about-container">
+                  <h3 className="title">About</h3>
+                  <p className="text">{user.bio}</p>
+                </div>
+
+                <SocialMedia />
+
+                <div className="buttons-wrap">
+                  <div className="first-wrap">
+                    <a
+                      href="mailto:mateus_vinicius15@outlook.com"
+                      target="blank"
+                      className="first"
+                    >
+                      Contact
+                    </a>
+                  </div>
+                  <div className="second-wrap">
+                    <a
+                      href="https://github.com/fariasmateuss?tab=repositories"
+                      target="blank"
+                      className="second"
+                    >
+                      Projects
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="right">
-              <div>
-                <h3 className="number">{user?.public_repos}</h3>
-                <h3 className="number-title">Repositories</h3>
-              </div>
-              <div>
-                <h3 className="number">{user?.following}</h3>
-                <h3 className="number-title">Following</h3>
-              </div>
-              <div>
-                <h3 className="number">{user?.followers}</h3>
-                <h3 className="number-title">Followers</h3>
+              <div className="right">
+                <div>
+                  <h3 className="number">{user.public_repos}</h3>
+                  <h3 className="number-title">Repositories</h3>
+                </div>
+                <div>
+                  <h3 className="number">{user.following}</h3>
+                  <h3 className="number-title">Following</h3>
+                </div>
+                <div>
+                  <h3 className="number">{user.followers}</h3>
+                  <h3 className="number-title">Followers</h3>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </main>
   );
 }
