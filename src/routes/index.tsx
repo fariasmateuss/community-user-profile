518421700bcd77694010ad289c3be407af7bb9da
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-01-13 00:18:43 -0400

    Upgrade to React Router v6

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Profile from '../pages/Profile';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact to="/" component={Profile} />
-        <Redirect to="/" />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   );
 }
