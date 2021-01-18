30e37b251a406f8705cdf0840228013f1bab34a2
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-01-18 00:25:59 -0400

    Create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+import { HeaderProps } from './types';
+
+import './styles.css';
+
+function Header({ returnToProfile }: HeaderProps) {
+  return (
+    <header className="wrapper-header">
+      <h1 className="topic">
+        Discovory Projects{' '}
+        <span role="img" aria-label="Astronaut">
+          👨‍🚀
+        </span>
+      </h1>
+
+      {returnToProfile && (
+        <Link className="anchor-return" to="/">
+          <FiChevronLeft size={16} />
+          Return
+        </Link>
+      )}
+    </header>
+  );
+}
+
+export default Header;
