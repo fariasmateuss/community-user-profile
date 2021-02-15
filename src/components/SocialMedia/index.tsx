b73c627d1c1fc138fa40551460bc6fc2a2668c12
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-02-15 02:54:38 -0400

    Move social medias data to its own file

--- src/components/SocialMedia/index.tsx
+++ src/components/SocialMedia/index.tsx
@@ -1,49 +1,15 @@
 import React from 'react';
 
-import {
-  AiFillGithub,
-  AiFillLinkedin,
-  AiFillYoutube,
-  AiFillMediumSquare,
-  AiFillTwitterSquare,
-} from 'react-icons/ai';
-import { SiDevDotTo } from 'react-icons/si';
+import { NavigationItemProps } from './types';
 
-import { NavigationItemProps, NavigationItems } from './types';
+import networks from '../../data/networks';
 
 import './styles.css';
-
-const socialMedias: NavigationItems = [
-  {
-    icon: <AiFillGithub />,
-    url: 'https://github.com/fariasmateuss/',
-  },
-  {
-    icon: <AiFillLinkedin />,
-    url: 'https://www.linkedin.com/in/fariasmateuss/',
-  },
-  {
-    icon: <AiFillMediumSquare />,
-    url: 'https://medium.com/@fariasmateuss',
-  },
-  {
-    icon: <AiFillTwitterSquare />,
-    url: 'https://twitter.com/fariasmateuss',
-  },
-  {
-    icon: <SiDevDotTo />,
-    url: 'https://dev.to/fariasmateuss',
-  },
-  {
-    icon: <AiFillYoutube />,
-    url: 'https://www.youtube.com/channel/UC2dD4ZlE8zuMoVGOP3jdB8g',
-  },
-];
 
 function SocialMedia() {
   return (
     <nav className="icons-container">
-      {socialMedias.map(({ icon, url }: NavigationItemProps, index) => (
+      {networks.map(({ icon, url }: NavigationItemProps, index) => (
         <a key={String(index)} href={url} target="blank" className="icon">
           {icon}
         </a>
