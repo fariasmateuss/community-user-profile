1333d1fba7bb2be2032bf24bb37e4f3e34d0b6c7
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-02-14 23:24:43 -0400

    Fix the name of the repositories hook

--- src/containers/Repositories/index.tsx
+++ src/containers/Repositories/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Repository from '../../components/Repository';
 import Header from '../../components/Header';
 
-import useRepositories from '../../hooks/userRepositories';
+import useRepositories from '../../hooks/useRepositories';
 
 import './styles.css';
 
