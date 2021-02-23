28a7000fe8ec6359a205028da67c9d87b70b161b
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-02-22 22:07:02 -0400

    Fetch API data with hook useArticles

---
+++ src/hooks/useArticles/index.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+import { Articles } from '../../shared/types/devto';
+
+import { USERNAME_KEY } from '../../constants/params';
+
+import api from '../../services/devto.api';
+
+function useArticlesData() {
+  const [articles, setArticles] = useState<Articles[]>([]);
+
+  useEffect(() => {
+    api.get(`articles?username=${USERNAME_KEY}`).then(response => {
+      setArticles(response.data);
+    });
+  }, []);
+  return articles;
+}
+
+export default useArticlesData;
