68da7461f5a9d9eb11e48f18457c7eafc167eba2
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-02-23 01:23:47 -0400

    Add user type for the article

--- src/shared/types/devto.ts
+++ src/shared/types/devto.ts
@@ -3,4 +3,9 @@ export interface Articles {
   title: string;
   social_image: string;
   canonical_url: string;
+  readable_publish_date: string;
+  user: {
+    name: string;
+    profile_image_90: string;
+  };
 }
