3d07943f189e32d559521a49c5b2d84c93d8e5ac
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-01-15 03:00:21 -0400

    Implement SEO with next-seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import { DefaultSeo } from 'next-seo';
 
-import GlobalStyle from '../styles/global';
-import { theme } from '../styles/theme';
+import { theme } from '@styles/theme';
+import GlobalStyle from '@styles/global';
+
+import SEO from '../../next-seo.config';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <>
+      <DefaultSeo {...SEO} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   );
 }
