b0812ccc21a1a26d04d756f46e27b0161e542297
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-01-19 15:30:16 -0400

    Remove unused utility getLanguageColor

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -33,31 +33,6 @@ a {
 }
 
 :root {
-  --typescript: #2b7489;
-  --javascript: #f1e05a;
-  --shell: #89e051;
-  --css: #563d7c;
-  --dart: #00b4ab;
-  --scss: #c6538c;
-  --dockerfile: #384d54;
-  --html: #e34c26;
-  --rust: #dea584;
-  --clojure: #db5855;
-  --c: #555555;
-  --swift: #ffac45;
-  --vue: #2c3e50;
-  --java: #b07219;
-  --php: #4F5D95;
-  --go: #375eab;
-  --ruby: #701516;
-  --objective-c: #438eff;
-  --kotlin: #F18E33;
-  --elixir: #6e4a7e;
-  --python: #3572A5;
-  --haskell: #29b544;
-  --arduino: #bd79d1;
-  --other: #9e9e9e;
-
   --toastify-color-progress-light:
     linear-gradient(135deg, #9580ff 0,#80ffea 100%)
   }
