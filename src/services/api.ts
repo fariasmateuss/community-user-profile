52a34b3c8f9d0ab590979ce471deb405d8c7afd7
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-01-23 21:33:14 -0400

    Create footer component

--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: `https://api.github.com/`,
-  headers: {
-    Authorization: `Bearer ${process.env.PRIVATE_TOKEN}`,
-  },
-});
